926b649be19f4faf4b4e5038c8a04ec2b469101d
Alexei Guevara <alexguev@DellaLu.local>
2012-11-20 00:14:18 -0500

    added more css styles

--- src/tasking/views/main.clj
+++ src/tasking/views/main.clj
@@ -3,18 +3,33 @@
   (:use [noir.core :only [defpage defpartial]]
         [hiccup.core :only [html]]))
 
-(defpartial render-story [{:keys [title]}]
+;create more partials for sections of UI
+
+(defpartial render-name [name]
+  [:div {:class "row remove-bottom"}
+   [:div {:class "one column"}]
+   [:div {:class "fourteen columns"}
+    [:div.story-box-title name]]])
+
+(defpartial render-task [{:keys [name start end owner size]}]
+  (println name)
+  [:div {:class "row remove-bottom"}
+   [:div {:class "one column"} "&nbsp;"]
+   [:div {:class "one column story-box-task-start"} start]
+   [:div {:class "one column story-box-task-end"} end]
+   [:div {:class "one column story-box-task-owner"} owner]
+   [:div {:class "eight columns story-box-task-name"} name]
+   [:div {:class "one column story-box-task-size"} size]])
+
+(defpartial render-story [{:keys [title] :as story}]
   [:div {:class "row remove-bottom"}
    [:div {:class "sixteen columns"}
     [:div.story-box
-     [:div {:class "row remove-bottom"}
-      [:div {:class "one column"}] [:div {:class "fourteen columns"} [:div.story-box-title title]]]
-     [:div {:class "row remove-bottom"}
-      [:div {:class "four columns"} "&nbsp;"] [:div {:class "eight columns"} "Task 1 fg fgdf gdf gdfgdf gdg fgdf gdf gfd gdf gdf gdfg dfg df g df g dfg df gdf gdf gdf gdf gdfg dfg dfgdgdfgdf gdf g df g df g df gdf gdf gdf gdf"]]
-     [:div {:class "row remove-bottom"}
-      [:div {:class "four columns"} "&nbsp;"] [:div {:class "eight columns"} "Task 2"]]]]])
+     (render-name title)
+     (for [task (:tasks story)] (render-task task))]]])
 
 (defpage "/" []
   (common/layout
-   (render-story {:title "Story 1"})))
+   (render-story {:title "Story 1" :tasks [{:name "Task 1-1" :start 20 :end 22 :owner "AG" :size 1.5}
+                                           {:name "Task 1-2" :start 20 :end 22 :owner "AG" :size 1.5}]})))
 
